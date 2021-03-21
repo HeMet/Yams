ad10724a851a98884791f7218100e7bc361b5c54
Hemet <hemet.mail@gmail.com>
2021-03-21 19:16:32 +0400

    Pass additional flag on Windows only

--- Package.swift
+++ Package.swift
@@ -1,13 +1,18 @@
 // swift-tools-version:5.0
 import PackageDescription
 
+let cyamlTarget: Target = .target(name: "CYaml")
+#if os(Windows)
+    cyamlTarget.cSettings = [.define("YAML_DECLARE_STATIC")]
+#endif
+
 let package = Package(
     name: "Yams",
     products: [
         .library(name: "Yams", targets: ["Yams"])
     ],
     targets: [
-        .target(name: "CYaml", cSettings: [.define("YAML_DECLARE_STATIC")]),
+        cyamlTarget,
         .target(name: "Yams", dependencies: ["CYaml"]),
         .testTarget(name: "YamsTests", dependencies: ["Yams"])
     ]
